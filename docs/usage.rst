63243739807f162259dd9fb93aa7bad43d0e4f6a
tingyu00 <zoezhang0408@gmail.com>
2021-02-23 12:34:11 -0800

    initial package setup

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use pypkgs_TingZ in a project::
+
+    import pypkgs_tingz
---
+++ tests/test_pypkgs_tingz.py
@@ -0,0 +1,5 @@
+from pypkgs_tingz import __version__
+from pypkgs_tingz import pypkgs_tingz
+
+def test_version():
+    assert __version__ == '0.1.0'
